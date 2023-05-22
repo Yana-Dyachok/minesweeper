13c24b340247dd81cec5f68fb59ae50ddb514a64
Yana-Dyachok <annetart1994@gmail.com>
2023-05-22 13:12:29 +0300

    feat: add burger menu in js file

---
+++ minesweeper/src/utils/burger.js
@@ -0,0 +1,31 @@
+import {
+    menuBurger,
+    nav,
+    overlay,
+  } from "./create.js";
+
+const body = document.querySelector("body");
+const navLink = document.querySelectorAll(".nav-list-item");
+
+menuBurger.addEventListener("click", () => {
+  menuBurger.classList.toggle("-active");
+  nav.classList.toggle("-active");
+  overlay.classList.toggle("-active");
+  body.classList.toggle("-active");
+});
+
+overlay.addEventListener("click", () => {
+  menuBurger.classList.remove("-active");
+  nav.classList.remove("-active");
+  overlay.classList.remove("-active");
+  body.classList.toggle("-active");
+});
+
+navLink.forEach((el) => {
+  el.addEventListener("click", () => {
+    menuBurger.classList.remove("-active");
+    nav.classList.remove("-active");
+    overlay.classList.remove("-active");
+    body.classList.remove("-active");
+  });
+});
