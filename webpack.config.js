24a66af44eaa36d32f0b2c91412d165945ffd24c
Yana-Dyachok <annetart1994@gmail.com>
2023-05-22 18:53:40 +0300

    feat: add eslintrc

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.export={
+    entry:"./src/index.js",
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: "main.js"
+    },
+    mode: "development",
+    devServer: {
+        compress: true,
+        port:3000,
+    },
+}
